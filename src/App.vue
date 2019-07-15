3da9027212d540d9f43690f725b0320125a05e66
cjyiz <cjyiz@aliyun.com>
2019-07-15 17:42:18 +0800

    创建button,icon组n组件

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <g-button class='btn' icon-position='right'  icon='left'>这是基础按钮</g-button>
+<g-icon name=left>图标</g-icon>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Button from '@/components/button'
+import Icon from '@/components/icon'
+export default{
+  components:{
+      'g-button':Button,
+      'g-icon':Icon
+  }
 }
-#nav {
-  padding: 30px;
-}
+</script>
+<style lang='scss' scoped>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/button.vue
@@ -0,0 +1,86 @@
+<template>
+  <button class="cj-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
+    <g-icon class="loading icon" v-if='loading' name="loading"></g-icon>
+    <g-icon class="icon" :name="icon" v-if="icon&& !loading"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
+</template>
+<script>
+import Icon from "@/components/icon.vue";
+export default {
+  name: "Cj-button",
+  components: {
+    "g-icon": Icon
+  },
+  props: {
+    icon: {},
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        return value === "left" || value === "right";
+      }
+    }
+  }
+};
+</script>
+<style lang='scss' scoped >
+@import "@/assets/main.scss";
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+.loading {
+  animation: spin 1s infinite linear;
+}
+.cj-button {
+  font-size: $font-size;
+  height: $button-height;
+  padding: 0.3em 1em;
+  border-radius: $border-radius;
+  border: 1px solid $border-color;
+  vertical-align: middle;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  background-color: $button-bg;
+  &:hover {
+    border-color: $border-color-hover;
+  }
+  &:active {
+    background-color: $button-active-bg;
+  }
+  &:focus {
+    outline: none;
+  }
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-left: 0.3em;
+      margin-right: 0;
+    }
+    > .content {
+      order: 1;
+    }
+  }
+}
+</style>
+
+
